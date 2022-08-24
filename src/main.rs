8da28c28a111fa969cade964e8bddb5a192cbb33
Stan Konnov <stanislav@jointheview.com>
2022-08-24 20:37:11 +0200

    a link to diesel article

--- src/main.rs
+++ src/main.rs
@@ -8,3 +8,9 @@ use handlers::index_handler;
 fn rocket() -> _ {
     rocket::build().mount("/", routes![index_handler])
 }
+
+/*
+On diesel:
+
+https://betterprogramming.pub/using-the-diesel-orm-for-a-web-app-with-rocket-90e610f6a6cf
+*/
