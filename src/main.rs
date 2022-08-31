62c1343a095a7af8294facd03d01a88f6549d7b2
Stan Konnov <stanislav@jointheview.com>
2022-08-31 13:59:31 +0200

    clean up handler

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,3 @@ use handlers::index_handler;
 fn rocket() -> _ {
     rocket::build().mount("/", routes![index_handler])
 }
-
-/*
-On diesel:
-
-https://betterprogramming.pub/using-the-diesel-orm-for-a-web-app-with-rocket-90e610f6a6cf
-*/
