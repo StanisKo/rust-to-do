6105196a91aeb901e789f384906d195f39e3b598
Stan Konnov <stanislav@jointheview.com>
2022-09-02 14:15:53 +0200

    just basic implementation without error handling

--- src/controllers/create_todo_item.rs
+++ src/controllers/create_todo_item.rs
@@ -3,11 +3,11 @@ use rocket::serde::json::Json;
 use crate::models::{TodoItem, NewTodoItem};
 use crate::services::todo_item_service;
 
-#[get("/todo-item/create", data="<new_todo_item>")]
+#[post("/todo-item/create", data="<new_todo_item>")]
 pub fn create_todo_item_controller(new_todo_item: Json<NewTodoItem>) -> Json<TodoItem> {
     let todo_item_to_insert = new_todo_item.into_inner();
 
-    let created_todo_item = todo_item_service::create_todo_item(todo_item_to_insert);
+    let created_todo_item = todo_item_service::create_todo_item(todo_item_to_insert).unwrap();
 
-    unimplemented!();
+    Json(created_todo_item)
 }
