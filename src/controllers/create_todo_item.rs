7a26109485a56f5f85f69afd78b80d9ba507c661
Stan Konnov <stanislav@jointheview.com>
2022-09-02 16:09:08 +0200

    tapping into rocket types

--- src/controllers/create_todo_item.rs
+++ src/controllers/create_todo_item.rs
@@ -1,13 +1,15 @@
 use rocket::serde::json::Json;
+use rocket::http::Status;
+use rocket::response::status::Custom;
 
 use crate::models::{TodoItem, NewTodoItem};
 use crate::services::todo_item_service;
 
 #[post("/create", data="<new_todo_item>")]
-pub fn create_todo_item_controller(new_todo_item: Json<NewTodoItem>) -> Json<TodoItem> {
+pub fn create_todo_item_controller(new_todo_item: Json<NewTodoItem>) -> Custom<Json<TodoItem>> {
     let todo_item_to_insert = new_todo_item.into_inner();
 
     let created_todo_item = todo_item_service::create_todo_item(todo_item_to_insert).unwrap();
 
-    Json(created_todo_item)
+    Custom(Status::Created, Json(created_todo_item))
 }
